7e84aa2d12e9a1fa3c88b369aff043c43bd4b947
Jonathan Hsiao <=>
2020-08-14 16:23:11 -0700

    pre deploy

--- server.js
+++ server.js
@@ -14,18 +14,22 @@ const { initializeAuthentication } = require("./auth");
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
-const port = `${process.env.SERVER_PORT}`;
+const port = `${process.env.PORT}`;
 app.use(urlencoded({extended: true}));
 app.use(json());
 app.use(cookieParser());
-
 app.use(passport.initialize());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 router(app);
 initializeAuthentication(app);
 
 app.get("/", (req, res) => {
     res.status(200).json({ data: "API running" })
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
