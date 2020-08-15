3ef630448c5df49bf1e28a8fd1938c5e42c4b0fe
Jonathan Hsiao <=>
2020-08-15 10:30:14 -0700

    require https

--- server.js
+++ server.js
@@ -20,17 +20,26 @@ app.use(json());
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, "client", "build")));
-app.enable("trust proxy");
+app.enable("trust proxy"); //heroku handles https, node never sees req.connection.encrypted so thinks it is undefined
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV == "production") {
+      return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+  }
+app.use(requireHTTPS);
 
 router(app);
 initializeAuthentication(app);
 
 app.get("/", (req, res) => {
-    res.status(200).json({ data: "API running" })
+    res.status(200).json({ data: "API running" });
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
